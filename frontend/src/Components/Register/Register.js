d98aefde5938518baee68b355b69cf778fbeadcd
Gad Jacobs <gadexofficial@gmail.com>
2019-06-01 11:27:54 +0100

    Register and signin components

---
+++ frontend/src/Components/Register/Register.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+
+class Register extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      name: ''
+    };
+  }
+
+  render() {
+    return (
+<article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
+        <main className="pa4 black-80">
+          <div className="measure">
+            <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+              <legend className="f1 fw6 ph0 mh0">Register</legend>
+              <div className="mt3">
+                <label className="db fw6 lh-copy f6" htmlFor="name">
+                  Name
+                </label>
+                <input
+                  className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                                type="text"
+                                name="name"
+                                id="indown teu "                />
+              </div>
+              <div className="mt3">
+                <label className="db fw6 lh-copy f6" htmlFor="email-address">
+                  Email
+                </label>
+                <input
+                  className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                  type="email"
+                  name="email-address"
+                  id="email-address"
+                />
+              </div>
+              <div className="mv3">
+                <label className="db fw6 lh-copy f6" htmlFor="password">
+                  Password
+                </label>
+                <input
+                  className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                  type="password"
+                  name="password"
+                  id="password"
+                />
+              </div>
+            </fieldset>
+            <div className="">
+              <input
+                className="br2 b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f5 dib"
+                type="submit"
+                value="Register"
+              />
+            </div>
+          </div>
+        </main>
+</article>
+       );
+    }
+}
---
+++ frontend/src/Components/SignIn/SignIn.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+
+class SignIn extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      signInEmail: '',
+      signInPassword: ''
+    };
+  }
+  render() {
+    return (
+      <div>
+        <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
+        <main className="pa4 black-80">
+          <div className="measure">
+            <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+              <legend className="f1 fw6 ph0 mh0">Sign In</legend>
+              <div className="mt3">
+                <label className="db fw6 lh-copy f6" htmlFor="email-address">
+                  Email
+                </label>
+                <input
+                  className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                  type="email"
+                  name="email-address"
+                  id="email-address"
+                />
+              </div>
+              <div className="mv3">
+                <label className="db fw6 lh-copy f6" htmlFor="password">
+                  Password
+                </label>
+                <input
+                  className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                  type="password"
+                  name="password"
+                  id="password"
+                />
+              </div>
+              <label className="pa0 ma0 lh-copy f6 pointer">
+                <input type="checkbox" /> Remember me
+              </label>
+            </fieldset>
+            <div className="">
+              <input
+                className="br2 b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f5 dib"
+                type="submit"
+                value="Sign in"
+              />
+            </div>
+            <div className="lh-copy mt3">
+              <p className="f6 link dim black db pointer">Register</p>
+            </div>
+          </div>
+        </main>
+      </article>
+      </div>
+    );
+  }
+}
+
+export default SignIn;
