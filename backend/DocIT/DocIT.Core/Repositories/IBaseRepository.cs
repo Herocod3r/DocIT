4224dc92ea033e6cdabb03d576720f1d1d058050
Herocod3r <jethromain@gmail.com>
2019-06-03 08:53:14 +0100

    Updated ibase repository

--- backend/DocIT/DocIT.Core/Repositories/IBaseRepository.cs
+++ backend/DocIT/DocIT.Core/Repositories/IBaseRepository.cs
@@ -1,7 +1,17 @@
 ﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+
 namespace DocIT.Core.Repositories
 {
-    public interface IBaseRepository
+    public interface IBaseRepository<Tmodel,Tid>
     {
+        Task<Tmodel> GetByIdAsync(Tid id);
+        Task<(List<Tmodel>, Int64)> ListAllAsync();
+        Task<Tmodel> CreateNewAsync(Tmodel item);
+        Task<Tmodel> UpdateAsync(Tmodel item);
+        Task DeleteAsync(Tmodel item);
+        
     }
 }
