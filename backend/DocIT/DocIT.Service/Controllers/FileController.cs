cafd28c6fa3a4837ca3343290053a67bec70a079
Herocod3r <jethromain@gmail.com>
2019-06-29 13:20:58 +0100

    completed pending endpoints

---
+++ backend/DocIT/DocIT.Service/Controllers/FileController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+using DocIT.Core.Services;
+using DocIT.Core.Data.Payloads;
+using DocIT.Core.Services.Exceptions;
+using Microsoft.AspNetCore.Http;
+
+namespace DocIT.Service.Controllers
+{
+    [Route("[controller]")]
+    public class FileController : BaseController
+    {
+        private readonly IFileUploader uploader;
+
+        public FileController(IFileUploader uploader)
+        {
+            this.uploader = uploader;
+        }
+       
+        [HttpPost]
+        public async Task<IActionResult> Post(IFormFile file) => Ok(await uploader.UploadFileAsync(file.OpenReadStream(), System.IO.Path.GetExtension(file.FileName)));
+
+        
+        
+    }
+}
